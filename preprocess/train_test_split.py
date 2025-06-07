fix(normalization correct), todo(pbs dans le shuffle, refaire le split)

preprocess/train_test_split.py
@@ -29,13 +29,23 @@ def _shuffle_indices(n_samples, random_state):
 
 def _stratified_split(y, n_test, random_state):
     label_to_indices = defaultdict(list)
+
+    # S'assurer que chaque label est hashable
     for idx, label in enumerate(y):
+        # Convertit les array scalaires (ex: array([1])) en int
+        if isinstance(label, np.ndarray):
+            if label.ndim == 0:
+                label = label.item()
+            elif label.shape == (1,):
+                label = label[0]
+            else:
+                raise ValueError("Les labels doivent être des scalaires ou des tableaux de forme (1,)")
         label_to_indices[label].append(idx)
 
     rng = np.random.default_rng(seed=random_state)
     train_idx, test_idx = [], []
 
-    for label, indices in label_to_indices.items():
+    for indices in label_to_indices.values():
         indices = np.array(indices)
         rng.shuffle(indices)
         n_label_test = int(len(indices) * (n_test / len(y)))

preprocessing.py
@@ -1,4 +1,5 @@
-from preprocess import Normalizer, train_test_split
+from preprocess.Normalizer import Normalizer
+from preprocess.train_test_split import train_test_split
 import pandas as pd
 import numpy as np
 import os
@@ -20,7 +21,7 @@ def split_dataset(df):
     return x, y
 
 def main():
-    try:
+    # try:
         if len(sys.argv) != 2:
             raise Exception("Usage: python preprocessing.py <path_to_dataset>")
         
@@ -42,12 +43,12 @@ def main():
         train_set = np.concatenate((X_train, Y_train), axis=1)
         test_set = np.concatenate((X_test, Y_test), axis=1)
 
-        np.savetxt('train_set.csv', train_set, delimiter=',', fmt='%f')
-        np.savetxt('test_set.csv', test_set, delimiter=',', fmt='%f')
+        np.savetxt('data/train_set.csv', train_set, delimiter=',', fmt='%f')
+        np.savetxt('data/test_set.csv', test_set, delimiter=',', fmt='%f')
 
-    except Exception as e:
-        print(f"Error: {e}")
-        sys.exit(1)
+    # except Exception as e:
+    #     print(f"Error: {e}")
+    #     sys.exit(1)
 
 if __name__ == "__main__":
     main()